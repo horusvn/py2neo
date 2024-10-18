40435b027245e083f5da12498c006a073d292d8f
Le Anh Duy <le.duy@horus.com.vn>
2024-10-18 18:14:34 +0700

    Add dist

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='py2neo',
+    version='0.0.1',
+    description='a pip-installable package example',
+    license='MIT',
+    packages=['py2neo'],
+    author='Duy Le',
+    author_email='le.duy@horus.com.vn',
+    keywords=['example'],
+    url='https://github.com/horusvn/py2neo'
+)
